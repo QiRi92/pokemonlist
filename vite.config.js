e4ab7576c8d568bb0ac5114d72c2324187b93d86
QiRi92 <abdulbaqi9201@gmail.com>
2025-08-05 22:24:42 +0800

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const repoName = 'pokemonlist'
 
 export default defineConfig({
   plugins: [vue()],
-  base: `/${repoName}/`,
+  base: `/pokemonlist/`,
   resolve: {
     alias: {
       '@': '/src'
